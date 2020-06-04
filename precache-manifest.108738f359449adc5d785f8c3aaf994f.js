0443947f1786b675da133e5b10e83849e256e201
inawi <laanoui.fst@uhp.ac.ma>
2020-06-04 23:13:28 +0100

    Updates

---
+++ precache-manifest.108738f359449adc5d785f8c3aaf994f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67f82a0889f588b110a6b946ccd3f3ee",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e53c0b6380a4493b953b",
+    "url": "/monsters-rolodex/static/css/main.790c9c51.chunk.css"
+  },
+  {
+    "revision": "12240f73676d8f2fbd85",
+    "url": "/monsters-rolodex/static/js/2.a3c82928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a3c82928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e53c0b6380a4493b953b",
+    "url": "/monsters-rolodex/static/js/main.e9220be8.chunk.js"
+  },
+  {
+    "revision": "6062e4a584c29c6e0372",
+    "url": "/monsters-rolodex/static/js/runtime-main.e3d252a9.js"
+  }
+]);
